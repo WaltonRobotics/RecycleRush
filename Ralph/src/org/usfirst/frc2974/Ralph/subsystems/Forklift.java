42dc336cb98661303a813fa2b275f96e244bd0bd
Armaan Joseph <armaan.joseph@waltonrobotics.org>
2015-01-31 13:24:32 -0500

    •	Added more methods, updated some preexisting ones with robot code •	Added Encoder and Limit Switch properly •	Created ForkliftZero command – sets elevator to default position •	Added forklift to Robot.java

--- Ralph/src/org/usfirst/frc2974/Ralph/subsystems/Forklift.java
+++ Ralph/src/org/usfirst/frc2974/Ralph/subsystems/Forklift.java
@@ -19,37 +19,68 @@ public class Forklift extends Subsystem
 	private boolean isHolding;
 	private boolean clawIsFullyOpen;
 	private boolean limitSwitchOn;
+	public final double NEUTRAL_POS = 6;//after elevator is zeroed, raises to this for safe driving
 	//TODO get input from limit switch
 	
 	CANTalon elevatorTalon = new CANTalon(0);
 	CANTalon clawTalon = new CANTalon(0);
 	// TODO getMotorReference 
-	Encoder liftEncoder = new Encoder(0, 0);
+	//Encoder liftEncoder = new Encoder(0, 0);
+	//DigitalInput limitSwitch = new DigitalInput(0);
 	
 	public Forklift()
 	{
+		//We need to remove/replace these placeholders 
 		isLowered = false;
 		isHolding = false;
 		clawIsFullyOpen = false;
 		limitSwitchOn = false;
+		elevatorTalon.ConfigRevLimitSwitchNormallyOpen(true);//assuming up=fwd, rev=down
+		elevatorTalon.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);//assuming quad encoder,#s get bigger as we go up
+	}
+	//These are methods used in ForkliftZero command
+	public void setPowerMode()
+	{
+		elevatorTalon.changeControlMode(CANTalon.ControlMode.PercentVbus);
+		//will there be brake functionality???
+	}
+	
+	public void setPositionMode()
+	{
+		elevatorTalon.changeControlMode(CANTalon.ControlMode.Position);		
+	}
+	
+	public boolean isZero()
+	{
+		return elevatorTalon.isRevLimitSwitchClosed();
+	}
+	
+	public void resetEncoder()
+	{
+		elevatorTalon.setPosition(0);
+	}	
+	
+	// <\methods used in ForkliftZero>
+	public void raiseToPosition(double pos)
+	{
+		elevatorTalon.set(pos);
 	}
 	
 	public void checkFullyOpen()
 	{
-		if(limitSwitchOn){
-			clawIsFullyOpen = true;
-		}
-		else {
-			clawIsFullyOpen = false;
-		}
+//		if(limitSwitchOn){
+//			clawIsFullyOpen = true;
+//		}
+//		else {
+//			clawIsFullyOpen = false;
+//		}
 	}
 	//raises/lowers by input number of levels(oneToteLevel*level)
+	//do we really need this one?-Armaan
 	public void raiseLowLevel(double level)
 	{
-		if(isLowered)
-			isLowered = false;
-		else if(!isLowered)
-			isLowered = true;
+		//setPositionMode();				
+		elevatorTalon.set(level);		
 	}
 	
 	//raises/lowers to a variable height(not dependent on levels)
@@ -63,7 +94,8 @@ public class Forklift extends Subsystem
 	//raises to max height(set # of levels?)
 	public void raiseToTop()
 	{
-
+		//elevatorTalon.changeControlMode(CANTalon.ControlMode.Position);
+		
 	}
 	
 	//same logic as raiseToTop
@@ -85,17 +117,24 @@ public class Forklift extends Subsystem
 	{
 		while(!clawIsFullyOpen){
 			
-			clawTalon.set(1);
+			clawTalon.set(.1);
 			checkFullyOpen();
 			if(clawIsFullyOpen)
 				break;
 		}
-		clawIsFullyOpen = false;
+		clawIsFullyOpen = true;
 	}
 	//close all the way
 	public void fullyClose()
 	{
-		
+		while(clawIsFullyOpen){
+			
+			clawTalon.set(-1);
+			checkFullyOpen();
+			if(!clawIsFullyOpen)
+				break;
+		}
+		clawIsFullyOpen = false;
 	}
 	
 	public void grab()
@@ -105,14 +144,15 @@ public class Forklift extends Subsystem
 	
 	public void drop()
 	{
-		lowerToBottom();
+		raiseLowVar(-.1);
+		clawTalon.set(-.1);
 		isHolding = false;
 	}
 	
 	public void resetClaw()
 	{
 		fullyOpen();
-		lowerToBottom();		
+		raiseLowVar(-.1);		
 	}
 	
 	/* Make stack a command
